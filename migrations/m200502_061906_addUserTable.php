044d83a9eeedb50cd362ec21155eeb84794f519a
and555a <and555a@intersvyaz.net>
2020-05-02 12:24:16 +0500

    init

---
+++ migrations/m200502_061906_addUserTable.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200502_061906_addUserTable
+ */
+class m200502_061906_addUserTable extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('users', [
+            'id' => $this->primaryKey(),
+            'login' => $this->string(),
+            'password' => $this->string()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('users');
+    }
+
+}
